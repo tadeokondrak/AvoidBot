dd25c62990739277442a71841dde166a847f727d
seraku24 <seraku24@users.noreply.github.com>
2017-02-14 05:26:08 -0800

    Refactor and add unit testing.
    - Set up gulp for task management.
    - Set up mocha/chai/sinon for BDD/expect-style testing.
    - Refactored logic into individual modules (one for general logic, another for Discord-specific logic).
    - Added unit testing for all logic.
    - Fixed a few bugs that showed up.
    - Added some comments.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/**
+ * main.js -- Program entry point.
+ */
+
 const Discord = require('discord.js');
 const client  = new Discord.Client();
 const config  = require('./config.json');
@@ -8,44 +12,4 @@ client.on('ready', () => {
   console.log('Bot started.');
 });
 
-client.on('message', message => {
-  if (message.author.bot || config.ignoredChannels.includes(message.channel.id)) {
-    return;
-  }
-  var stripped = stripURLs(message.content);
-  var fifthglyphs = detectFifthGlyphs(stripped);
-  if (fifthglyphs.totalCount > 8 || fifthglyphs.longestRun > 2 || fifthglyphs.bold) {  
-    message.channel.sendMessage('Too many fifthglyphs, or a bold glyph. Auto-kick.');
-    message.author.send('Too many fifthglyphs, or a bold glyph. Auto-kick.');
-    client.channels.get(config.logChannel).sendMessage(message.author + ' was kicked.');
-    message.member.kick();
-    return;
-  }
-  if (fifthglyphs.totalCount > 0 && !message.content.match(/@(?:here|everyone)/i)) {
-    message.reply('fifthglyph found: ' + replaceFifthGlyphs(stripped));
-  }
-});
-
-function stripURLs(text) {
-  const regexURL = /[a-z][\da-z+.-]*:\/\/(?:[\da-z._~%!$&'(*+,;=:@\/?-]|\)(?!\s))+/ig;
-  return text.replace(regexURL, '[link]');
-}
-
-function detectFifthGlyphs(text) {
-  const regex = /(\*\*)?[eĕêěɇėẹëèéēẽęæœɛɜəǝɘεеєэEĔÊĚɆĖẸËÈÉĒẼĘÆŒƐƏƎΕЕЄЭ€]+\1?/g;  
-  var matches = text.match(regex) || [];
-  var counts = matches.map(x => x.replace(/\*/g, '').length);
-  return {
-    totalCount: counts.reduce(function(a,b) { return a + b }, 0),
-    longestRun: Math.max.apply(null, counts),
-    bold: matches.filter(x => x.startsWith('*')).length > 0
-  }
-}
-
-function replaceFifthGlyphs(text) {
-  const regexSmall = /[eĕêěɇėẹëèéēẽęæœɛɜəǝɘεеєэ]/g;
-  const regexLarge = /[EĔÊĚɆĖẸËÈÉĒẼĘÆŒƐƏƎΕЕЄЭ€]/g;
-  return text.replace(regexSmall, '■').replace(regexLarge, '█');
-}
-
-
+client.on('message', require('./lib/avoid5-discord.js').handleMessage);
